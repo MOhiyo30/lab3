1e2d0a2f1f2eeeb98afed0bc8ec9f0caca7a092f
m.abdukaxxorova <m.abdukaxxorova@newuu.uz>
2025-02-07 08:57:32 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    //problem1
+    // int num1,num2,num3,num4,num5;
+    // cin>>num1>>num2>>num3>>num4>>num5;
+    //
+    // int sum=num1+num2+num3+num4+num5;
+    //
+    // cout<<"Sum is :"<<sum<<endl;
+
+
+//problem2
+//     int studentid;
+// float gpa;
+// int yearofstudy;
+// int fee;
+// cout<<"Enter your student id:"<<endl;
+// cin>>studentid;
+//
+// cout<<"Enter your gpa:"<<endl;
+// cin>>gpa;
+//
+//  cout<<"Enter your fee:"<<endl;
+//     cin>>fee;
+//
+//     cout<<"Enter your year of study:"<<endl;
+//     cin>>yearofstudy;
+// cout<<"Student ID:"<<studentid<<endl;
+
+    //problem3
+    double celsius, fahrenheit;
+cout<<"Enter temperature in ceisius:"<<endl;
+    cin>>celsius;
+
+    
+
+
+
+
+
+
+
+    return 0;
+ }

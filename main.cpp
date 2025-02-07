5a3a0dd37bb20dc3175afc04bec6fff0c348291e
MOhiyo30 <m.abdukaxxorova@newuu.uz>
2025-02-07 09:05:42 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -35,6 +35,83 @@ cout<<"Enter temperature in ceisius:"<<endl;
     cin>>celsius;
 
     
+// problem4
+ //    double cost;
+ // int quantity;
+ //    cout<<"Enter your cost:"<<endl;
+ //    cin>>cost;
+ //    cout<<"Enter your quantity:"<<endl;
+ //    cin>>quantity;
+ //    int totalCost = cost*quantity;
+ //    cout<<"TotalCost is :";
+
+
+// problem5
+// int age1,age2,age3,age4;
+//     float average_age;
+//     cout<<"Enter your age1:";
+//     cin>>age1;
+//     cout<<"Enter your age2:";
+//     cin>>age2;
+//     cout<<"Enter your age3:";
+//     cin>>age3;
+//     cout<<"Enter your age4:";
+//     cin>>age4;
+//     average_age=(age1+age2+age3+age4)/4;
+//     cout<<"Average age is:"<<average_age<<endl;
+
+// problem6
+ // int length,width,area,perimeter;
+ //    cout<<"Enter length:";
+ //    cin>>length;
+ //    cout<<"Enter width:";
+ //    cin>>width;
+ //    area = length*width;
+ //    perimeter = 2*(length+width);
+ //    cout<<"Area of rectangle:"<<area<<endl;
+ //    cout<<"Perimeter of rectangle:"<<perimeter<<endl;
+
+// problem7
+    // int devidend,devisor,quotient,remainder;
+    // cout<<"Enter dividend:";
+    // cin>>dividend;
+    // cout<<"Enter divisor: ";
+    // cin>>divisor;
+    // quotient = dividend/divisor;
+    // remainder= dividend%divisor;
+    // cout<<"quotient:"<<quotient<<endl;
+    // cout<<"Remainder:"<<remainder<<endl;
+
+
+// problem8
+// double usd_amount,exchange_rate,uzs_amount;
+//     cout<<"Enter amount in usd:";
+//     cin>>usd_amount;
+//     cout<<"Enter exchange rate(1 usd to uzs)";
+//     cin>>exchange_rate;
+//     uzs_amount = usd_amount*exchange_rate;
+//     cout<<"Equivalent amount in uzs:"<<uzs_amount<<endl;
+
+
+
+// problem9
+// double radius,area;
+//     cout<<"Enter radius:";
+//     cin>>radius;
+// area=3.14*radius*radius;
+//     cout<<"Area of a circle:"<<area<<endl;
+
+
+
+// problem10
+// double radius, height,volume;
+//     cout<<"Enter radius:";
+//     cin>>radius;
+//     cout<<"Enter height:";
+//     cin>>height;
+//     volume=3.14*radius*radius*height;
+//     cout<<"Volume :"<<volume<<endl;
+
 
 
 
